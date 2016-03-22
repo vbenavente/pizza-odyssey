e980eea2c81e5fe21874a6777e1ebe912783c936
vbenavente <vbenavente@hotmail.com>
2016-03-22 12:41:08 -0700

    one working object!

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
+var hoursArray = [];
 var getRandomIntInclusive;
-var myArray = [];
-
+var driversRecommended;
 var eightAm, nineAm, tenAm, elevenAm, noon, onePm, twoPm, threePm, fourPm, fivePm, sixPm, sevenPm, eightPm, ninePm, tenPm, elevenPm, midnight, oneAm;
+var pizzasDelivered1;
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var pizzaSoldBlock1 = function () {
-  Math.floor((Math.random() + 4) + 1);
-  console.log('pizzaSoldBlock1');
+eightAm = {
+  pizzasSold: getRandomIntInclusive(0,4),
+  pizzasDelivered: getRandomIntInclusive(0,4),
 };
-pizzaSoldBlock1();
+var DriversRecommended = eightAm.pizzasDelivered;
+eightAm.DriversRec = Math.ceil(DriversRecommended / 3);
 
-var pizzaDeliveriesBlock1 = function() {
-  Math.floor((Math.random() + 4) + 1);
-  console.log('pizzaDeliveriesBlock1');
-};
-pizzaDeliveriesBlock1();
-
-var Ballard = {
-  hour: '8:00',
-  pizzaSold: 'pizzaSoldBlock1',
-  deliveries: 'PizzaDeliveriesBlock1',
-  driversRec: function(){
-    if(deliveries > 0) {
-      console.log('Drivers Recommended: ' + (Math.ceil(deliveries / 3)));
-    } else {
-      console.log('No Drivers Recomended');
-    }
-  }
-};
-Ballard();
+hoursArray.push(eightAm, nineAm, tenAm, elevenAm, noon, onePm, twoPm, threePm, fourPm, fivePm, sixPm, sevenPm, eightPm, ninePm, tenPm, elevenPm, midnight, oneAm);
