58d4c5e816710331fd88a025d556ec67a75c659e
vbenavente <vbenavente@hotmail.com>
2016-03-23 11:49:29 -0700

    started to create table generator

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-var getRandomInclusive;
 var cityLocation;
 var ballard, firstHill, theIntDist, southLakeUnion, georgeTown, Ravenna;
-var hourlyData;
 
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function cityLocation(name){
-  this.name = 'Ballard';
+  this.name = name;
   this.hourlyData = [];
 }
 
@@ -19,13 +17,42 @@ cityLocation.prototype.pushhourlyData = function(data){
 function hourlyData(time, minPizzasSold, maxPizzasSold, minDeliveriesMade, maxDeliveriesMade){
   this.time = time;
   this.PizzasSold = getRandomIntInclusive(minPizzasSold, maxPizzasSold);
-  this.deliveriesMade = getRandomInclusive(minDeliveriesMade, maxDeliveriesMade);
+  this.deliveriesMade = getRandomIntInclusive(minDeliveriesMade, maxDeliveriesMade);
   this.driversNeeded = Math.ceil(this.deliveriesMade / 3);
 }
 
 ballard = new cityLocation('Ballard');
-ballard.pushHourlyData(new hourlyData('8:00am', 0, 3, 1, 7));
-ballard.pushHourlyData(new hourlyData('9:00am', 0, 3, 1, 7));
-ballard.pushHourlyData(new hourlyData('10:00am', 0, 3, 1, 7));
-ballard.pushHourlyData(new hourlyData('11:00am', 5, 10, 2, 8));
-ballard.pushHourlyData(new hourlyData('noon', 5, 10, 2, 8));
+ballard.pushhourlyData(new hourlyData('8:00am', 0, 3, 1, 7));
+ballard.pushhourlyData(new hourlyData('9:00am', 0, 3, 1, 7));
+ballard.pushhourlyData(new hourlyData('10:00am', 0, 3, 1, 7));
+ballard.pushhourlyData(new hourlyData('11:00am', 5, 10, 2, 8));
+ballard.pushhourlyData(new hourlyData('noon', 5, 10, 2, 8));
+
+console.log ('running');
+
+function generateDataRow(inputArray){
+  var row = document.createElement('tr');
+  var col;
+  for(var i = 0; i < inputArray.length; i++){
+    col = document.createElement('td');
+    col.textContent = inputArray[i];
+    row.appendChild(col);
+  }
+  return row;
+}
+
+function generateHeadingRow(inputArray){
+  var row = document.createElement('tr');
+  var col;
+  for(var i = 0; i < inputArray.length; i++){
+    col = document.createElement('th');
+    col.textContent = inputArray[i];
+    row.appendChild(col);
+  }
+  return row;
+}
+
+var pizzaTable = document.createElement('table');
+
+var firstRow = generateHeadingRow(['Time', 'Pizzas Sold', 'Pizzas Delivered', 'Drivers Recommended']);
+var secondRow = generateDataRow([hourlyData])
