2a915e8d984bf71414006c66566fd6941273a00b
vbenavente <vbenavente@hotmail.com>
2016-03-22 19:38:15 -0700

    working Ballard 8 and 9!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var getRandomIntInclusive;
 var driversRecommended;
 var ballard, firstHill, theInternationalDistrict, southLakeUnion, georgeTown, ravenna;
 var eightAm, nineAm, tenAm, elevenAm, noon, onePm, twoPm, threePm, fourPm, fivePm, sixPm, sevenPm, eightPm, ninePm, tenPm, elevenPm, midnight, oneAm;
-var pizzasDelivered1;
+var what;
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -13,13 +13,27 @@ eightAm = {
   pizzasSold: getRandomIntInclusive(0,4),
   pizzasDelivered: getRandomIntInclusive(0,4),
 };
-driversRecommended = eightAm.pizzasDelivered;
-eightAm.driversRec = Math.ceil(driversRecommended / 3);
 
-hoursArray.push(eightAm);//, nineAm, tenAm, elevenAm, noon, onePm, twoPm, threePm, fourPm, fivePm, sixPm, sevenPm, eightPm, ninePm, tenPm, elevenPm, midnight, oneAm);
+eightAm.driversRec = (Math.ceil(eightAm.pizzasDelivered / 3));
 
-//for(var i = 0; i < hoursArray.length; i++);
-var ballardUl = document.getElementById('Ballard');
-var cityBallard = document.createElement('li');
-cityBallard.textContent = hoursArray[0].time + 'am ' + hoursArray[0].pizzasSold + ' pizzas ' + hoursArray[0].pizzasDelivered + ' deliveries ' + '-- [drivers recommended: ' + hoursArray[0].driversRec + ']';
-ballardUl.appendChild(cityBallard);
+nineAm = {
+  time: '9:00',
+  pizzasSold: getRandomIntInclusive(0,4),
+  pizzasDelivered: getRandomIntInclusive(0,4),
+};
+
+nineAm.driversRec = (Math.ceil(nineAm.pizzasDelivered / 3));
+
+hoursArray.push(eightAm, nineAm);//, tenAm, elevenAm, noon, onePm, twoPm, threePm, fourPm, fivePm, sixPm, sevenPm, eightPm, ninePm, tenPm, elevenPm, midnight, oneAm);
+
+for(var i = 0; i < hoursArray.length; i++) {
+  var ballardUl = document.getElementById('Ballard');
+  var cityBallard = document.createElement('li');
+  if(hoursArray[i].driversRec < 1) {
+    what = '[driver not recommended]';
+  }else {
+    what = ' [drivers recommended: ' + hoursArray[i].driversRec + ']';
+  }
+  cityBallard.textContent = hoursArray[i].time + 'am ' + hoursArray[i].pizzasSold + ' pizzas ' + hoursArray[i].pizzasDelivered + ' deliveries ' + what;
+  ballardUl.appendChild(cityBallard);
+}
