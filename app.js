ded6a7aad10c337ffe52f70061da9f43bc791ca0
vbenavente <vbenavente@hotmail.com>
2016-03-23 18:38:34 -0700

    index.html formating and app.js pizzassold counter

--- app.js
+++ app.js
@@ -189,7 +189,11 @@ for(var i = 0; i < hoursArrayBallard.length; i++) {
   }
   cityBallard.textContent = hoursArrayBallard[i].time + ' ' + hoursArrayBallard[i].pizzasSold + ' pizzas ' + hoursArrayBallard[i].pizzasDelivered + ' deliveries ' + what;
   ballardArt.appendChild(cityBallard);
+
+  var pizzaOdysseysBal = hoursArrayBallard[0].pizzasSold + hoursArrayBallard[1].pizzasSold + hoursArrayBallard[2].pizzasSold + hoursArrayBallard[3].pizzasSold + hoursArrayBallard[4].pizzasSold + hoursArrayBallard[5].pizzasSold + hoursArrayBallard[6].pizzasSold + hoursArrayBallard[7].pizzasSold + hoursArrayBallard[8].pizzasSold + hoursArrayBallard[9].pizzasSold + hoursArrayBallard[10].pizzasSold + hoursArrayBallard[11].pizzasSold + hoursArrayBallard[12].pizzasSold + hoursArrayBallard[13].pizzasSold + hoursArrayBallard[14].pizzasSold + hoursArrayBallard[15].pizzasSold + hoursArrayBallard[16].pizzasSold + hoursArrayBallard[17].pizzasSold;
+
 }
+console.log(pizzaOdysseysBal);
 
 for(var i = 0; i < hoursArrayFirstHill.length; i++) {
   var firstHillArt = document.getElementById('FirstHill');
@@ -201,7 +205,11 @@ for(var i = 0; i < hoursArrayFirstHill.length; i++) {
   }
   cityFirstHill.textContent = hoursArrayFirstHill[i].time + ' ' + hoursArrayFirstHill[i].pizzasSold + ' pizzas ' + hoursArrayFirstHill[i].pizzasDelivered + ' deliveries ' + what;
   firstHillArt.appendChild(cityFirstHill);
+
+  var pizzaOdysseysFH = hoursArrayFirstHill[0].pizzasSold + hoursArrayFirstHill[1].pizzasSold + hoursArrayFirstHill[2].pizzasSold + hoursArrayFirstHill[3].pizzasSold + hoursArrayFirstHill[4].pizzasSold + hoursArrayFirstHill[5].pizzasSold + hoursArrayFirstHill[6].pizzasSold + hoursArrayFirstHill[7].pizzasSold + hoursArrayFirstHill[8].pizzasSold + hoursArrayFirstHill[9].pizzasSold + hoursArrayFirstHill[10].pizzasSold + hoursArrayFirstHill[11].pizzasSold + hoursArrayFirstHill[12].pizzasSold + hoursArrayFirstHill[13].pizzasSold + hoursArrayFirstHill[14].pizzasSold + hoursArrayFirstHill[15].pizzasSold + hoursArrayFirstHill[16].pizzasSold + hoursArrayFirstHill[17].pizzasSold;
 }
+
+console.log(pizzaOdysseysFH);
 
 for(var i = 0; i < hoursArrayIntDist.length; i++) {
   var intDistArt = document.getElementById('InternationalDistrict');
@@ -213,7 +221,11 @@ for(var i = 0; i < hoursArrayIntDist.length; i++) {
   }
   cityIntDist.textContent = hoursArrayIntDist[i].time + ' ' + hoursArrayIntDist[i].pizzasSold + ' pizzas ' + hoursArrayIntDist[i].pizzasDelivered + ' deliveries ' + what;
   intDistArt.appendChild(cityIntDist);
+
+  var pizzaOdysseysIntD = hoursArrayIntDist[0].pizzasSold + hoursArrayIntDist[1].pizzasSold + hoursArrayIntDist[2].pizzasSold + hoursArrayIntDist[3].pizzasSold + hoursArrayIntDist[4].pizzasSold + hoursArrayIntDist[5].pizzasSold + hoursArrayIntDist[6].pizzasSold + hoursArrayIntDist[7].pizzasSold + hoursArrayIntDist[8].pizzasSold + hoursArrayIntDist[9].pizzasSold + hoursArrayIntDist[10].pizzasSold + hoursArrayIntDist[11].pizzasSold + hoursArrayIntDist[12].pizzasSold + hoursArrayIntDist[13].pizzasSold + hoursArrayIntDist[14].pizzasSold + hoursArrayIntDist[15].pizzasSold + hoursArrayIntDist[16].pizzasSold + hoursArrayIntDist[17].pizzasSold;
 }
+
+console.log(pizzaOdysseysIntD);
 
 for(var i = 0; i < hoursArraySLU.length; i++) {
   var sLUArt = document.getElementById('SouthLakeUnion');
@@ -225,7 +237,11 @@ for(var i = 0; i < hoursArraySLU.length; i++) {
   }
   citySLU.textContent = hoursArraySLU[i].time + ' ' + hoursArraySLU[i].pizzasSold + ' pizzas ' + hoursArraySLU[i].pizzasDelivered + ' deliveries ' + what;
   sLUArt.appendChild(citySLU);
+
+  var pizzaOdysseysSLU = hoursArraySLU[0].pizzasSold + hoursArraySLU[1].pizzasSold + hoursArraySLU[2].pizzasSold + hoursArraySLU[3].pizzasSold + hoursArraySLU[4].pizzasSold + hoursArraySLU[5].pizzasSold + hoursArraySLU[6].pizzasSold + hoursArraySLU[7].pizzasSold + hoursArraySLU[8].pizzasSold + hoursArraySLU[9].pizzasSold + hoursArraySLU[10].pizzasSold + hoursArraySLU[11].pizzasSold + hoursArraySLU[12].pizzasSold + hoursArraySLU[13].pizzasSold + hoursArraySLU[14].pizzasSold + hoursArraySLU[15].pizzasSold + hoursArraySLU[16].pizzasSold + hoursArraySLU[17].pizzasSold;
 }
+
+console.log(pizzaOdysseysSLU);
 
 for(var i = 0; i < hoursArrayGeorgeTown.length; i++) {
   var georgeTownArt = document.getElementById('Georgetown');
@@ -237,7 +253,11 @@ for(var i = 0; i < hoursArrayGeorgeTown.length; i++) {
   }
   cityGeorgeTown.textContent = hoursArrayGeorgeTown[i].time + ' ' + hoursArrayGeorgeTown[i].pizzasSold + ' pizzas ' + hoursArrayGeorgeTown[i].pizzasDelivered + ' deliveries ' + what;
   georgeTownArt.appendChild(cityGeorgeTown);
+
+  var pizzaOdysseysGT = hoursArrayGeorgeTown[0].pizzasSold + hoursArrayGeorgeTown[1].pizzasSold + hoursArrayGeorgeTown[2].pizzasSold + hoursArrayGeorgeTown[3].pizzasSold + hoursArrayGeorgeTown[4].pizzasSold + hoursArrayGeorgeTown[5].pizzasSold + hoursArrayGeorgeTown[6].pizzasSold + hoursArrayGeorgeTown[7].pizzasSold + hoursArrayGeorgeTown[8].pizzasSold + hoursArrayGeorgeTown[9].pizzasSold + hoursArrayGeorgeTown[10].pizzasSold + hoursArrayGeorgeTown[11].pizzasSold + hoursArrayGeorgeTown[12].pizzasSold + hoursArrayGeorgeTown[13].pizzasSold + hoursArrayGeorgeTown[14].pizzasSold + hoursArrayGeorgeTown[15].pizzasSold + hoursArrayGeorgeTown[16].pizzasSold + hoursArrayGeorgeTown[17].pizzasSold;
 }
+
+console.log(pizzaOdysseysGT);
 
 for(var i = 0; i < hoursArrayRavenna.length; i++) {
   var ravennaArt = document.getElementById('Ravenna');
@@ -249,4 +269,10 @@ for(var i = 0; i < hoursArrayRavenna.length; i++) {
   }
   cityRavenna.textContent = hoursArrayRavenna[i].time + ' ' + hoursArrayRavenna[i].pizzasSold + ' pizzas ' + hoursArrayRavenna[i].pizzasDelivered + ' deliveries ' + what;
   ravennaArt.appendChild(cityRavenna);
+
+  var pizzaOdysseysRav = hoursArrayRavenna[0].pizzasSold + hoursArrayRavenna[1].pizzasSold + hoursArrayRavenna[2].pizzasSold + hoursArrayRavenna[3].pizzasSold + hoursArrayRavenna[4].pizzasSold + hoursArrayRavenna[5].pizzasSold + hoursArrayRavenna[6].pizzasSold + hoursArrayRavenna[7].pizzasSold + hoursArrayRavenna[8].pizzasSold + hoursArrayRavenna[9].pizzasSold + hoursArrayRavenna[10].pizzasSold + hoursArrayRavenna[11].pizzasSold + hoursArrayRavenna[12].pizzasSold + hoursArrayRavenna[13].pizzasSold + hoursArrayRavenna[14].pizzasSold + hoursArrayRavenna[15].pizzasSold + hoursArrayRavenna[16].pizzasSold + hoursArrayRavenna[17].pizzasSold;
 }
+
+console.log(pizzaOdysseysRav);
+
+var totalPizzaOysseys = ((pizzaOdysseysBal + pizzaOdysseysFH + pizzaOdysseysIntD + pizzaOdysseysSLU + pizzaOdysseysGT + pizzaOdysseysRav) * 6);
