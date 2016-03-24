9d9c12f589654147d53c9886a17513560117dccd
vbenavente <vbenavente@hotmail.com>
2016-03-24 01:13:37 -0700

    ballard table appears!

--- app.js
+++ app.js
@@ -16,20 +16,31 @@ CityLocation.prototype.pushHourlyData = function(data){
 
 function HourlyData(time, minPizzasSold, maxPizzasSold, minDeliveriesMade, maxDeliveriesMade){
   this.time = time;
-  this.PizzasSold = getRandomIntInclusive(minPizzasSold, maxPizzasSold);
+  this.pizzasSold = getRandomIntInclusive(minPizzasSold, maxPizzasSold);
   this.deliveriesMade = getRandomIntInclusive(minDeliveriesMade, maxDeliveriesMade);
   this.driversNeeded = Math.ceil(this.deliveriesMade / 3);
 }
 
 ballard = new CityLocation('ballard');
-var eight = new HourlyData('8:00am', 0, 3, 1, 7);
-ballard.pushHourlyData(eight);
-var nine = new HourlyData('9:00am', 0, 3, 1, 7);
-ballard.pushHourlyData(nine);
-var ten = new HourlyData('10:00am', 0, 3, 1, 7);
-ballard.pushHourlyData(ten);
-var eleven = new HourlyData('11:00am', 0, 4, 0, 4);
-ballard.pushHourlyData(eleven);
+ballard.pushHourlyData(new HourlyData('8:00am', 0, 3, 1, 7));
+ballard.pushHourlyData(new HourlyData('9:00am', 0, 3, 1, 7));
+ballard.pushHourlyData(new HourlyData('10:00am', 0, 3, 1, 7));
+ballard.pushHourlyData(new HourlyData('11:00am', 5, 10, 2, 8));
+ballard.pushHourlyData(new HourlyData('noon', 5, 10, 2, 8));
+ballard.pushHourlyData(new HourlyData('1:00pm', 5, 10, 2, 8));
+ballard.pushHourlyData(new HourlyData('2:00pm', 2, 13, 1, 7));
+ballard.pushHourlyData(new HourlyData('3:00pm', 2, 13, 1, 7));
+ballard.pushHourlyData(new HourlyData('4:00pm', 2, 13, 1, 7));
+ballard.pushHourlyData(new HourlyData('5:00pm', 0, 15, 2, 9));
+ballard.pushHourlyData(new HourlyData('6:00pm', 0, 15, 2, 9));
+ballard.pushHourlyData(new HourlyData('7:00pm', 0, 15, 2, 9));
+ballard.pushHourlyData(new HourlyData('8:00pm', 1, 3, 4, 12));
+ballard.pushHourlyData(new HourlyData('9:00pm', 1, 3, 4, 12));
+ballard.pushHourlyData(new HourlyData('10:00pm', 1, 3, 4, 12));
+ballard.pushHourlyData(new HourlyData('11:00pm', 8, 15, 6, 16));
+ballard.pushHourlyData(new HourlyData('midnight', 8, 15, 6, 16));
+ballard.pushHourlyData(new HourlyData('1:00am', 8, 15, 6, 16));
+
 console.log ('ballard.HourlyData');
 
 //var columndata = ['a', 'b', 'c', 1, 2, 3];
@@ -46,30 +57,13 @@ function generateDataRow(inputArray){
   return row;
 }
 
-function generateHeadingRow(inputArray){
-  var row = document.createElement('tr');
-  var col;
-  for(var i = 0; i < inputArray.length; i++){
-    col = document.createElement('td');
-    col.textContent = inputArray[i];
-    row.appendChild(col);
+function makeTable(store, storeId){
+
+  var pizzaTable = document.getElementById(storeId);
+  for(var i = 0; i < store.hourlyLocationData.length; i++) {
+    var fancyRow = generateDataRow([store.hourlyLocationData[i].time, store.hourlyLocationData[i].pizzasSold, store.hourlyLocationData[i].deliveriesMade, store.hourlyLocationData[i].driversNeeded]);
+    pizzaTable.appendChild(fancyRow);
   }
-  return row;
 }
 
-var pizzaTable = document.getElementById('ballard');
-
-var firstRow = generateHeadingRow(['Time', 'Pizzas Sold', 'Pizzas Delivered', 'Drivers Recommended']);
-
-pizzaTable.appendChild(firstRow);
-
-for(var i = 0; i < ballard.hourlyLocationData.length; i++) {
-  var fancyRow = generateDataRow([ballard.hourlyLocationData[i].time, ballard.hourlyLocationData[i].pizzasSold, ballard.hourlyLocationData[i].deliveriesMade, ballard.hourlyLocationData[i].driversNeeded]);
-
-  pizzaTable.appendChild(fancyRow);
-}
-//var thirdRow = generateDataRow([ballard.hourlyLocationData[1].time, ballard.hourlyLocationData[1].pizzasSold, ballard.hourlyLocationData[1].deliveriesMade, ballard.hourlyLocationData[1].driversNeeded]);
-//var fourthRow = generateDataRow([ballard.hourlyLocationData[2].time, ballard.hourlyLocationData[2].pizzasSold, ballard.hourlyLocationData[2].deliveriesMade, ballard.hourlyLocationData[2].driversNeeded]);
-// var fifthRow = generateDataRow(ballard.hourlyLocationData[3].time, ballard.hourlyLocationData[3].pizzasSold, ballard.hourlyLocationData[3].deliveriesMade, ballard.hourlyLocationData[3].driversNeeded);
-// var sixthRow = generateDataRow(ballard.hourlyLocationData[4].time, ballard.hourlyLocationData[4].pizzasSold, ballard.hourlyLocationData[4].deliveriesMade, ballard.hourlyLocationData[4].driversNeeded);
-//document.getElementById('ballard').appendChild(pizzaTable);
+makeTable(ballard, 'ballard');
