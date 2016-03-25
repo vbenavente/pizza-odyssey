0ba9d450dea206a0fafd700442ab517ea08feb16
vbenavente <vbenavente@hotmail.com>
2016-03-25 00:30:50 -0700

    table header fixed js, cut th/tr from html

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var CityLocation;
 var ballard, firstHill, theIntDist, southLakeUnion, georgeTown, Ravenna;
+var pizzaTable;
+var pizzzaOdyssey;
 
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -8,6 +9,7 @@ function getRandomIntInclusive(min, max) {
 function CityLocation(name){
   this.name = name;
   this.hourlyLocationData = [];
+  this.HeaderData = [['Time'], ['Pizzas Sold'], ['Pizzas Delivered'], ['Drivers Recommended']];
 }
 
 CityLocation.prototype.pushHourlyData = function(data){
@@ -20,8 +22,7 @@ function HourlyData(time, minPizzasSold, maxPizzasSold, minDeliveriesMade, maxDe
   this.deliveriesMade = getRandomIntInclusive(minDeliveriesMade, maxDeliveriesMade);
   this.driversNeeded = Math.ceil(this.deliveriesMade / 3);
 }
-
-function getTime(e) {
+/*function getTime(e) {
   if(!e) {
     e = window.event;
   }
@@ -31,7 +32,9 @@ function timeEntry(e) {
   var time = document.getElementById('timefeedback');
   var hour = document.getElementById('hourentry');
   time.textContent = '';
-}
+}*/
+
+//var createblahblahblah = document.getElementById('get-')
 
 ballard = new CityLocation('ballard');
 ballard.pushHourlyData(new HourlyData('8:00am', 0, 3, 1, 7));
@@ -155,10 +158,35 @@ Ravenna.pushHourlyData(new HourlyData('11:00pm', 2, 4, 3, 11));
 Ravenna.pushHourlyData(new HourlyData('midnight', 2, 4, 3, 11));
 Ravenna.pushHourlyData(new HourlyData('1:00am', 2, 4, 3, 11));
 
+//function collectLocationData(event){
+//  event.preventDefault();}
+//Step 1 get data from inputs
+//Step 2 use data from for to create location objects with our constructor(already created)
+//Step 3 use our object to create a table
+//Step 4 put table on the page
+
+/*var locationName = event.target.getLocation.value;
+var firstTime = event.target.getTime.value;
+var minnPizza = event.target.minPizza.value;
+var maxxPizza = event.target.maxPizzza.value;
+var minnDel = event.target.minDel.value;
+var maxxDel = event.target.maxDel.value;
+console.log('locationName',locationName);
+console.log('firstTime', firstTime);
+console.log('minnPizza', minnPizza);
+console.log('maxxPizza', maxxPizza);
+console.log('minnDel', minnDel);
+console.log('maxxDel', maxDel);
+
+var userLocation = new Pizzalocatoin(locationName);
+var hourlyLocationData = new hourlyLocationData(firstTime, minnPizza, maxxPizza, minnDel, maxxDel);
+userLocation.pushhourlyLocationData(hourlyLocationData);
+console.log(userLocation);*/
+
 //var columndata = ['a', 'b', 'c', 1, 2, 3];
 //generateDataRow(columndata);
 
-function generateDataRow(inputArray){
+function generateHeadingRow(inputArray){
   var row = document.createElement('tr');
   var col;
   for(var i = 0; i < inputArray.length; i++){
@@ -169,33 +197,54 @@ function generateDataRow(inputArray){
   return row;
 }
 
-function wantPizzas(inputArray){
-  var odysseyrow = document.createElement('ul');
-  var odysseycol;
+function generateDataRow(inputArray){
+  var row = document.createElement('tr');
+  var col;
   for(var i = 0; i < inputArray.length; i++){
-    odysseycol = document.createElement('li');
-    odysseycol.textContent = inputArray[i];
-    odysseyrow.appendChild(odysseycol);
+    col = document.createElement('td');
+    col.textContent = inputArray[i];
+    row.appendChild(col);
   }
-  return odysseyrow;
+  return row;
+}
+
+function pizzaSoldArray(store){
+  var row = document.createElement('ul');
+  var col;
+  for(var i = 0; i < inputArry.length; i++){
+    col = document.createElement('li');
+    col.textContent = inputArray[i];
+    row.appendChild(col);
+  }
+  return row;
+}
+
+function totalPizzas(){
+  pizzzaOdyssey = document.getElementById('pizzaodysseys');
+  for(var i = 0; i < 18; i++){
+    var sumPizzas = pizzaSoldArray([ballard.hourlyLocationData[i].pizzasSold] + [firstHill.hourlyLocationData[i].pizzasSold]);
+    pizzzaOdyssey.appendChild(sumPizzas);
+  }
+  return sumPizzas;
 }
 
 function makeTable(store, storeId){
-
-  var pizzaTable = document.getElementById(storeId);
+  pizzaTable = document.getElementById(storeId);
+  var fancyHeader = generateHeadingRow([store.HeaderData[0], store.HeaderData[1], store.HeaderData[2], store.HeaderData[3]]);
+  pizzaTable.appendChild(fancyHeader);
   for(var i = 0; i < store.hourlyLocationData.length; i++) {
     var fancyRow = generateDataRow([store.hourlyLocationData[i].time, store.hourlyLocationData[i].pizzasSold, store.hourlyLocationData[i].deliveriesMade, store.hourlyLocationData[i].driversNeeded]);
     pizzaTable.appendChild(fancyRow);
+  }
 
-    var fancyPizzas = wantPizzas([store.hourlyLocationData[i].pizzasSold]);
-  }
-  function sumPizzas(fancyPizzas){
-    var totalPizzas = 0;
-    for(var i = 0; i < fancyPizzas.length; i++){
-      totalPizzas += fancyPizzas[i];
-    }
-    sumPizzas();
-  }
+    //var fancyPizzas = wantPizzas([store.hourlyLocationData[i].pizzasSold]);
+    //console.log(fancyPizzas);
+  //function sumPizzas(fancyPizzas){
+  //  var totalPizzas = 0;
+  //  for(var i = 0; i < fancyPizzas.length; i++){
+  //    totalPizzas += fancyPizzas[i];
+  //  }
+  //  sumPizzas();
 }
 
 makeTable(ballard, 'ballard');
